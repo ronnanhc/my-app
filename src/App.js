89a5a0b690051b354de7516f6ec3f71b9dd32b60
ronnanhc <ronna@nationalhealthclaim.com>
2022-11-07 11:46:32 -0700

    Assignment 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Expenses from './components/Expenses';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Expenses></Expenses>
+    </div>
+  );
+}
+
+export default App;
